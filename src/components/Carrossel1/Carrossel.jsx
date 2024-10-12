c619b093f6f8385fa598267f62f5e79cf9be5bd7
samuel chagas <72897243+samuel-chagas@users.noreply.github.com>
2024-10-12 18:10:44 -0300

    atualizaçao slide

--- src/components/Carrossel1/Carrossel.jsx
+++ src/components/Carrossel1/Carrossel.jsx
@@ -2,7 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.css'
 import './Carrossel.css'
 import Slides from './Slide';
-
+import Slide2 from './Slide2';
 function UncontrolledExample() {
   return (
     <Carousel>
@@ -12,12 +12,12 @@ function UncontrolledExample() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Slides />
+        <Slide2 />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Slides />
+        <Slide2 />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
