8da9d6d9d56f84c1fa2eff4d334ca59861c132f5
samuel chagas <72897243+samuel-chagas@users.noreply.github.com>
2024-11-25 17:23:14 -0300

    adiçao do codigo com o .env

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, 
   {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+  dialect: process.env.DB_DIALECT, // Adicione esta linha
   logging: console.log, // Habilita logs do Sequelize
 });
 
